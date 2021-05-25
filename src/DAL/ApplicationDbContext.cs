62aa7b5ce91185e765082bf02d44600528acebd5
Yurii Yanovytskyi <yurii.yanovytskyi@globallogic.com>
2021-05-25 15:14:50 +0300

    add dbContext

---
+++ src/DAL/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<City> Cities { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<City>().Property(c => c.Id).IsRequired();
+        }
+    }
+}
