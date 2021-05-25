851494fac81e128063ed64059df2421e9999a8db
Yurii Yanovytskyi <yurii.yanovytskyi@globallogic.com>
2021-05-25 15:14:23 +0300

    create City model

---
+++ src/DAL/Models/City.cs
@@ -0,0 +1,10 @@
+﻿namespace DAL
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
