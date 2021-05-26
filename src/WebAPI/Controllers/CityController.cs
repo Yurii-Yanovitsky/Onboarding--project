7e92e4899ffaccf45376450a0b841c2763605a81
Yurii Yanovytskyi <yurii.yanovytskyi@globallogic.com>
2021-05-26 10:40:07 +0300

    implement CityController

---
+++ src/WebAPI/Controllers/CityController.cs
@@ -0,0 +1,37 @@
+﻿using DAL;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CityController : Controller
+    {
+        private readonly ICityRepository _repository;
+
+        public CityController(ICityRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<City>>> GetAll()
+        {
+            return new ObjectResult(await _repository.GetAllCities());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<City>> Get(int id)
+        {
+            return await _repository.GetCityById(id);
+        }
+
+        [HttpPost]
+        public async Task Post(City[] cities)
+        {
+            await _repository.AddCities(cities);
+        }
+    }
+}
