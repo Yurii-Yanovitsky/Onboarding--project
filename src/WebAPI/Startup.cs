ef90ea25abe650c7effa7794dd4d78eda39af641
Yurii Yanovytskyi <yurii.yanovytskyi@globallogic.com>
2021-05-26 10:38:45 +0300

    register dependecies

--- src/WebAPI/Startup.cs
+++ src/WebAPI/Startup.cs
@@ -1,20 +1,27 @@
+using DAL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebAPI
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddScoped<ICityRepository, CityRepository>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
